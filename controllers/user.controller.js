3d9edee6f1a6839a0339cc9d1df5c8947ddd7747
20201157-TANAW\admin <earthlono123@gmail.com>
2023-01-24 21:00:18 +1300

    Implement get all users in the system

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -19,7 +19,23 @@ const getAllUsersData = async (req, res) => {
   }
 };
 
+const getUserDetails = async (req, res) => {
+  const userId = req.params.userId;
+   try{
+    const user = await User.findOne({_id: userId}, {password :0})
+    if(!user){
+      res
+        .status(400)
+        .json({ error: { msg: "This email is not exist!" } });
+    }
+    res.json(user);
+  }catch(err){
+    res.status(500).send("Server getUserDetails is error ");
+  }
+}
+
 module.exports = {
   getUserData,
   getAllUsersData,
+  getUserDetails
 };
--- routes/user.route.js
+++ routes/user.route.js
@@ -3,10 +3,12 @@ const router = express.Router();
 const {
   getUserData,
   getAllUsersData,
+  getUserDetails,
 } = require("../controllers/user.controller");
 const { authMiddleware } = require("../middleware/auth.middleware");
 
 router.get("/myData", [authMiddleware], getUserData);
 router.get("/allUsers", getAllUsersData);
+router.get("/details/:userId", getUserDetails);
 
 module.exports = router;
