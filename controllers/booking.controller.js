5b08a07838b4aa7930662dd7eb7ba9ad81e1f84c
Earth <earthlono123@gmail.com>
2023-07-01 21:20:48 +1200

    remove unuse code

--- controllers/booking.controller.js
+++ controllers/booking.controller.js
@@ -1,10 +1,8 @@
 const Booking = require("../models/booking.model");
 const User = require("../models/user.model");
 
-
 const addBooking = async (req, res) => {
   const id = req.user.id;
-  console.log("addBooking...");
   const { dateTime, adultAmount, babyAmount, specification } = req.body;
   try {
     const newBooking = new Booking({
@@ -17,33 +15,19 @@ const addBooking = async (req, res) => {
     const result = await newBooking.save();
     res.json(result);
   } catch (error) {
-    console.log("error in addBoking api", error);
     res.status(500).send("Server addBooking Error");
   }
 };
 const getBooking = async (req, res) => {
 const id = req.user.id;
   try {
-    console.log('work----')
-
-    const bookingDetails = await Booking.find({user:id}).populate('user', 'name email phoneNumber')
-
-    
-  //   const transformData = await Promise.all (bookingDetails.map(async(item) => {
-  //     const thisUser = await User.findOne({_id:item.user})
-  //     console.log('thisUser',thisUser)
-  //     return {
-  //  ...item,
-  //     clientInfo:{
-  //       name: thisUser.name,
-  //       email: thisUser.email,
-  //       phoneNumber: thisUser.phoneNumber,
-  //     }
-  //     }
-  //   }
-  //   ))
-    
-    console.log('bookingDetails',bookingDetails)
+    let bookingDetails
+    const thisUser = await User.findOne({user:id});
+    if(thisUser.role === "admin"){
+      bookingDetails = await Booking.find().populate('user', 'name email phoneNumber')   
+    }else{
+      bookingDetails = await Booking.find({user:id}).populate('user', 'name email phoneNumber')   
+    } 
     res.json(bookingDetails)
   } catch (error) {
     console.log("error in getBooking api", error);
@@ -51,12 +35,10 @@ const id = req.user.id;
   }
 };
 
-const getOrderById = async (req, res) => {
-  const {orderId} = req.params
-  console.log('orderID from params is', orderId)
+const getBookingById = async (req, res) => {
+  const {bookingId} = req.params
   try {
-    const bookingDetails = await Booking.findOne({_id:orderId})
-    console.log('this bookingDetails', bookingDetails)
+    const bookingDetails = await Booking.findOne({_id:bookingId}).populate('user', 'name email phoneNumber')
     res.json(bookingDetails);
     
   } catch (error) {
@@ -68,5 +50,5 @@ const getOrderById = async (req, res) => {
 module.exports = {
   addBooking,
   getBooking,
-  getOrderById
+  getBookingById,
 };
--- routes/booking.route.js
+++ routes/booking.route.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const { authMiddleware } = require("../middleware/auth.middleware");
-const {addBooking, getBooking,getOrderById} =  require("../controllers/booking.controller");
+const {addBooking, getBooking,getBookingById} =  require("../controllers/booking.controller");
 
 router.post("/add", [authMiddleware], addBooking);
 router.get("/get", [authMiddleware], getBooking);
-router.get("/getById/:orderId", [authMiddleware], getOrderById);
-
+router.get("/getById/:bookingId", [authMiddleware], getBookingById);
 
 module.exports = router;
 
